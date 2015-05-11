3829f62c00420f06bae4e84fad3876148fde8cda
Jan Andersson <jan.andersson@gmail.com>
2015-05-11 22:00:45 +0200

    Move out drawing code from Life

--- src/life.rs
+++ src/life.rs
@@ -22,17 +22,6 @@ impl Life {
         }
     }
 
-    fn draw(&self) {
-        for row in (0..self.height) {
-            for col in (0..self.width) {
-                let c = if self.get_at(col, row) { "\x1B[32mo\x1B[0m" } else { "." };
-                print!("{} ", c);
-            }
-            print!("\n");
-        }
-        print!("\n");
-    }
-
     fn get_at(&self, x: i16, y: i16) -> bool {
         if y < 0 || x < 0 || y >= self.height || x >= self.width {
             return false
@@ -79,13 +68,24 @@ fn clear_screen() {
     print!("\x1B[2J\x1B[0;0H");
 }
 
+fn draw(world: &Life) {
+    for row in (0..world.height) {
+        for col in (0..world.width) {
+            let c = if world.get_at(col, row) { "\x1B[32mo\x1B[0m" } else { "." };
+            print!("{} ", c);
+        }
+        print!("\n");
+    }
+    print!("\n");
+}
+
 fn main() {
     let mut world = Life::new(60, 30);
     world.randomize();
     loop {
         world = world.next();
         clear_screen();
-        world.draw();
+        draw(&world);
         thread::sleep_ms(250);
     }
 }
